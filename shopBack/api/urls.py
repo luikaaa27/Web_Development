dfb01bf68f823459605798860e3f7c4ec6ce7e9a
luikaaa27 <42840919+luikaaa27@users.noreply.github.com>
2020-03-29 16:24:03 +0500

    lab8

--- shopBack/api/urls.py
+++ shopBack/api/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('products/<int:pk>/',views.getProduct, name='product'),
     path('categories/', views.categories, name='categories'),
     path('categories/<int:pk>', views.getCategory, name='category'),
-    path('categories/<int:pk>/products', views.getCategory, name='categoryProducts'),
+    path('categories/<int:pk>/products', views.categoryProducts, name='categoryProducts'),
 ]
