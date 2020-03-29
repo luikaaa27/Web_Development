2ae33aae5c40c921ea290f6eb878c9f555e5525f
luikaaa27 <42840919+luikaaa27@users.noreply.github.com>
2020-03-29 16:28:16 +0500

    lab8

--- shopBack/api/models.py
+++ shopBack/api/models.py
@@ -3,8 +3,16 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=255)
+    
     def __str__(self):
-        return self.name
+        return '{}: {}'.format(self.id, self.name)
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name
+        }
+
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
@@ -13,5 +21,14 @@ class Product(models.Model):
     count = models.IntegerField()
     categoryId = models.ForeignKey(Category,on_delete=models.CASCADE)
 
-    def __str__(self):
-        return self.name
+     def __str__(self):
+        return '{}: {}'.format(self.id, self.name, self.price, self.description, self.count)
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'price': self.price,
+            'description': self.description,
+            'count': self.count
+        }
