86b7d86194f4d630057b669f3ca4fee40abfb3fe
luikaaa27 <42840919+luikaaa27@users.noreply.github.com>
2020-04-18 02:39:34 +0500

    lab10

---
+++ HeadHunter/hhFront/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {CompaniesComponent} from './companies/companies.component';
+import {VacanciesComponent} from './vacancies/vacancies.component';
+
+
+const routes: Routes = [
+  { path: '', component: CompaniesComponent},
+  { path: 'company/:id/vacancies', component: VacanciesComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
